d364d160c5e19f5aaa58284c1af6b9bf8bcbee1e
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-02-24 00:14:55 +0100

    Add a few commands

--- server/index.mjs
+++ server/index.mjs
@@ -67,8 +67,69 @@ cmd.register("revive", (player) => {
     player.spawn(player.pos.x, player.pos.y, player.pos.z, 0);
 });
 
+cmd.register("dimension", (player, args) => {
+    if (!player.getSyncedMeta("admin")) {
+        return cmd.sendChat(
+            player,
+            "{FF00FF}You don`t have enough permissions to use this command"
+        );
+    }
+
+    if (args.length > 0) {
+        const _player = alt.Player.all.filter((p) => p.name === args[0])[0];
+
+        _player.dimension = Number(args[1]);
+    }
+});
+
+cmd.register("tp", (player, args) => {
+    if (!player.getSyncedMeta("admin")) {
+        return cmd.sendChat(
+            player,
+            "{FF00FF}You don`t have enough permissions to use this command"
+        );
+    }
+
+    if (args && !args[0]) {
+        cmd.sendChat(player, "Usage: /tp (target player)");
+        return;
+    }
+
+    const foundPlayers = alt.Player.all.filter((p) => p.name === args[0]);
+
+    if (foundPlayers && foundPlayers.length > 0) {
+        player.pos = foundPlayers[0].pos;
+        cmd.sendChat(
+            player,
+            `You got teleported to {1cacd4}${foundPlayers[0].name}{ffffff}`
+        );
+    } else {
+        cmd.sendChat(
+            player,
+            `{ff0000} Player {ff9500}${args[0]} {ff0000}not found..`
+        );
+    }
+});
+
 cmd.register("logout", (player) => {
-    alt.emitClient(player, "core:client:login");
+    character.logout(player);
+});
+
+cmd.register("kick", (player, args) => {
+    if (!player.getMeta("admin")) {
+        return cmd.sendChat(
+            player,
+            "{FF00FF}You don`t have enough permissions to use this command"
+        );
+    }
+
+    if (args.length > 0) {
+        const _player = alt.Player.all.filter((p) => p.name === args[0])[0];
+
+        cmd.sendChat(_player, `{FF0000}You were kicked from the server`);
+        cmd.broadcastChat(`{5555AA}${_player.name} {FFFFFF}kicked`);
+        _player.kick("You were kicked from the server");
+    }
 });
 
 // cmd.register("poweroff", player => {
