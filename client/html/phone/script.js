f1e272fc8f337ae31c2d726ab6478d8bee361700
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-02-25 00:08:00 +0100

    Update phone

--- client/html/phone/script.js
+++ client/html/phone/script.js
@@ -1,4 +1,30 @@
 window.onload = () => {
+    const content = document.querySelector("div.phone div.phone-content"),
+        iframe = document.querySelector("iframe");
+
+    loadHome();
+
     if ("alt" in window) {
+    }
+
+    function loadHome() {
+        loadBackground();
+
+        iframe.src = "home.html";
+
+        content.innerHTML = iframe.contentWindow.document;
+    }
+
+    function loadApp(app) {
+        iframe.src = `apps/${app}.html`;
+
+        content.innerHTML = iframe.contentWindow.document;
+    }
+
+    function loadBackground() {
+        content.style["background-image"] =
+            "url(https://media.idownloadblog.com/wp-content/uploads/2021/09/Light_Beams_Blue_Light-iPhone-13-Pro-official-apple-wallpaper-710x1536.jpg)";
+        content.style["background-size"] = "cover";
+        content.style["background-position"] = "center";
     }
 };
