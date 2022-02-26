452376be2fc18630404001dbd5952a30a6db59c9
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-02-26 03:03:24 +0100

    Oops

--- server/localization.mjs
+++ server/localization.mjs
@@ -1,6 +1,8 @@
 import config from "./../config.json";
 
-const translation = import(`./../localization/${config.language}.json`);
+const translation = import(
+    `./../localization/${config.language.replace("en_US", "src")}/default.json`
+);
 
 export function getString(string) {
     return translation[string] || string;
