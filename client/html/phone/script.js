2080047149dcb82a899a4ded641b4b76a64a5f00
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-02-28 03:50:29 +0100

    Add clock, signals

--- client/html/phone/script.js
+++ client/html/phone/script.js
@@ -22,6 +22,8 @@ window.onload = () => {
                         loadApp(app.getAttribute("data-app"));
                     });
                 });
+
+                loadNav();
             });
     }
 
@@ -38,6 +40,8 @@ window.onload = () => {
                 document.getElementById(
                     "appScript"
                 ).href = `apps/${app}/app.js`;
+
+                loadNav();
             });
     }
 
@@ -46,5 +50,57 @@ window.onload = () => {
             "url(https://media.idownloadblog.com/wp-content/uploads/2021/09/Light_Beams_Blue_Light-iPhone-13-Pro-official-apple-wallpaper-710x1536.jpg)";
         content.style["background-size"] = "cover";
         content.style["background-position"] = "center";
+    }
+
+    function loadNav() {
+        const nav = document.createElement("div");
+
+        nav.classList.add("nav");
+
+        nav.addEventListener("click", loadHome);
+
+        content.appendChild(nav);
+
+        loadSignals();
+        loadClock();
+    }
+
+    function loadClock() {
+        const clock = document.createElement("div");
+
+        clock.classList.add("clock");
+
+        let date = new Date().toLocaleTimeString().split(":");
+
+        date = `${date[0]}:${date[1]}`;
+
+        clock.innerHTML = date;
+
+        content.appendChild(clock);
+
+        setInterval(() => {
+            if (!clock) clearInterval(this);
+            else {
+                date = new Date().toLocaleTimeString().split(":");
+                date = `${date[0]}:${date[1]}`;
+
+                clock.innerHTML = date;
+            }
+        }, 1000);
+    }
+
+    function loadSignals() {
+        const signals = document.createElement("div"),
+            wifi = document.createElement("i"),
+            battery = document.createElement("i");
+
+        signals.classList.add("signals");
+        wifi.classList.add("wifi", "fa-solid", "fa-wifi");
+        battery.classList.add("battery", "fa-solid", "fa-battery-full");
+
+        signals.appendChild(wifi);
+        signals.appendChild(battery);
+
+        content.appendChild(signals);
     }
 };
