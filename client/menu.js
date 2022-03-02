809736f507078cdf5fd7ea1214c007824e30b6b1
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-03-03 00:39:27 +0100

    Menu client

---
+++ client/menu.js
@@ -0,0 +1,54 @@
+import * as alt from "alt-client";
+
+const menus = [];
+
+/**
+ * @param {Object} data
+ * @returns {string}
+ */
+export function createMenu(data) {
+    const menuId = generateMenuId(),
+        menu = {};
+
+    menu.id = menuId;
+    menu.title = data.title;
+    menu.items = data.items;
+    menu.webview = new alt.WebView(
+        "http://resource/client/html/menu/index.html"
+    );
+
+    menus.push(menu);
+
+    alt.showCursor(true);
+    alt.toggleGameControls(false);
+    menu.webview.focus();
+    menu.webview.emit("webview:load", menu);
+
+    menu.webview.on("webview:closeMenu", () => {
+        closeMenu(menuId);
+    });
+
+    menu.webview.on("webview:emitServer", (e, args) => {
+        alt.emitServer(e, args);
+    });
+
+    return menuId;
+}
+
+export function closeMenu(menuId) {
+    const menu = menus.find((m) => m.id === menuId);
+
+    if (menu) {
+        alt.showCursor(false);
+        alt.toggleGameControls(true);
+        menu.webview?.unfocus();
+        menu.webview?.destroy();
+        menus.splice(menus.indexOf(menu), 1);
+    }
+}
+
+function generateMenuId() {
+    return Math.random().toString(36).substring(2, 10);
+}
+
+alt.onServer("core:client:menu:create", createMenu);
